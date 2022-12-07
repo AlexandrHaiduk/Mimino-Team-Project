5947f1b0f94204288dd660da9885e561f54bcc31
andriitomilov <andriitomilov@ukr.net>
2022-12-07 14:04:24 +0200

    add slider-mini-mod

--- src/js/slider-mini-mod.js
+++ src/js/slider-mini-mod.js
@@ -1,23 +1,19 @@
 const swiper = new Swiper('.swiper-mini-mod', {
-  // speed: 400,
-  spaceBetween: 25,
+  slidesPerView: 'auto',
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next-mini',
-    prevEl: '.swiper-button-prev-mini',
+    nextEl: '.swiper-button-next-mini-mod',
+    prevEl: '.swiper-button-prev-mini-mod',
   },
 
   breakpoints: {
     320: {
-      slidesPerView: 1.3,
       spaceBetween: 20,
     },
     1024: {
-      slidesPerView: 2.5,
       spaceBetween: 34,
     },
     1280: {
-      slidesPerView: 3,
       spaceBetween: 86,
     },
   },
